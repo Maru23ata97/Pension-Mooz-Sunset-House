0cc22ba666b8e6a879bfea2c70ed41b902fdcfc3
Maru23ata97 <118214748+Maru23ata97@users.noreply.github.com>
2022-11-28 09:43:53 -1000

    1 file modified

--- App.tsx
+++ App.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import './style.css';
 import Slider from './components/Slider';
 import Navbar from './components/Navbar';
-import Footer from './components/Footer';
 
 export default function App() {
   return (
